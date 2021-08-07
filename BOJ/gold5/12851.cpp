c99e5d4d050592a33bf6c00eba95e46d422ddca9
GyeonghoYang <chchch1213@naver.com>
2021-08-07 10:24:41 +0000

    Upgrade 12851

--- BOJ/gold5/12851.cpp
+++ BOJ/gold5/12851.cpp
@@ -1,17 +1,12 @@
 // #include <cstdio> // For Test
 #include <queue>
-#include <vector>
 #include <iostream>
 using namespace std;
 
-struct node
-{
-    int time, pos;
-};
-
-int n, k, prevTime = -1, counts[2][100001] = {0};
-
-queue<node> nodes;
+const int MAX = 1e6;
+int n, k, dist[MAX + 1] = {0};
+int times = -1, count = 0;
+queue<int> q;
 
 int main(int argc, char *argv[])
 {
@@ -24,55 +19,33 @@ int main(int argc, char *argv[])
     cin >> n >> k;
 
     // Process
-    int *curCounts, *futureCounts;
-    counts[0][n] = 1;
-    nodes.push({0, n});
-    while (!nodes.empty())
+    q.push(n);
+    while (!count)
     {
-        auto node = nodes.front();
-        nodes.pop();
-
-        // 새로운 시간을 탐색 시작할 때
-        if (node.time > prevTime)
+        int size = q.size();
+        while (size--)
         {
-            // 새로 채울 곳 횟수 초기화
-            curCounts = counts[node.time % 2];
-            futureCounts = counts[!(node.time % 2)];
-            fill_n(futureCounts, 100001, 0);
+            int x = q.front();
+            q.pop();
 
-            // 이전 시간 업데이트
-            prevTime = node.time;
+            if (x == k)
+                count++;
+            for (int nx : {x - 1, x + 1, x * 2})
+            {
+                if (nx < 0 || nx > MAX)
+                    continue;
+                if (dist[nx] && dist[nx] != dist[x] + 1)
+                    continue;
 
-            // 찾았을 경우 탐색 종료
-            if (curCounts[k] > 0)
-                break;
+                q.push(nx);
+                dist[nx] = dist[x] + 1;
+            }
         }
-
-        // 걷기
-        node.time++;
-        if (node.pos > 0)
-        {
-            if (futureCounts[node.pos - 1] == 0)
-                nodes.push({node.time, node.pos - 1});
-            futureCounts[node.pos - 1] += curCounts[node.pos];
-        }
-        if (node.pos < k && node.pos < 100000)
-        {
-            if (futureCounts[node.pos + 1] == 0)
-                nodes.push({node.time, node.pos + 1});
-            futureCounts[node.pos + 1] += curCounts[node.pos];
-        }
-        // 순간이동
-        if (node.pos < k && node.pos <= 50000)
-        {
-            if (futureCounts[node.pos * 2] == 0)
-                nodes.push({node.time, node.pos * 2});
-            futureCounts[node.pos * 2] += curCounts[node.pos];
-        }
+        times++;
     }
 
+    cout << times << '\n'
+         << count << '\n';
     // Output
-    cout << prevTime << '\n'
-         << curCounts[k] << '\n';
     return 0;
 }
