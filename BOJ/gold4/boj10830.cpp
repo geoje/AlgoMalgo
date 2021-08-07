3be2949fcb2113b276deb6d01736e0fba8e466fe
GyeongHo Yang <chchch1213@naver.com>
2021-08-07 13:38:53 +0900

    continue 10830

--- BOJ/gold4/boj10830.cpp
+++ BOJ/gold4/boj10830.cpp
@@ -1,22 +1,52 @@
-// #include <cstdio> // For Test
+#include <cstdio> // For Test
+#include <vector>
 #include <iostream>
 using namespace std;
 
-int n;
+typedef vector<vector<int>> matrix;
+matrix operator*(matrix &a, matrix &b)
+{
+    int s = a.size();
+    matrix c(s, vector<int>(s));
+    for (int i = 0; i < s; i++)
+        for (int j = 0; j < s; j++)
+            for (int k = 0; k < s; k++)
+                c[i][j] += a[i][k] * b[k][j];
+    return c;
+}
+
+int n, b;
+matrix ans, base;
 
 int main(int argc, char *argv[])
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    // freopen(argv[1], "r", stdin); // For Test
+    freopen(argv[1], "r", stdin); // For Test
 
     // Input
-    cin >> n;
+    cin >> n >> b;
+    ans.assign(n, vector<int>(n));
+    base.assign(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+    {
+        ans[i][i] = 1; // 항등 행렬 만들기
+        for (int j = 0; j < n; j++)
+            cin >> base[i][j];
+    }
 
     // Process
+    for (; b > 0; b /= 2)
+    {
+        if (b & 1)
+            ans = ans * base;
+        base = base * base;
+    }
 
     // Output
-    cout << n << '\n';
+    for (int i = 0; i < n; i++, cout << '\n')
+        for (int j = 0; j < n; j++)
+            cout << ans[i][j] << ' ';
     return 0;
 }
