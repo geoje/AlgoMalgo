dfd67a10424d054083595abaa005d14c4b000674
GyeonghoYang <chchch1213@naver.com>
2021-08-17 13:30:42 +0900

    fin kakao 2021-s2

--- Kakao/internship/2021-s2.cpp
+++ Kakao/internship/2021-s2.cpp
@@ -4,32 +4,49 @@
 #include <queue>
 using namespace std;
 
-struct node
-{
-    int r, c, m;
-};
-
 const pair<int, int> dirs[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
 vector<int> solution(vector<vector<string>> places)
 {
     vector<int> answer;
+    int r, c, ans;
 
-    for (auto place : places)
+    for (auto place : places) // 모든 입력
     {
-        // 초기 사람 위치 전부 넣기
-        queue<node> q;
-        for (int i = 0; i < 5; i++)
-            for (int j = 0; j < 5; j++)
-                if (place[i][j] == 'P')
-                    q.push({i, j, 0});
+        ans = 1;
+        for (int i = 0; i < 5; i++)        // 모든 y 위치
+            for (int j = 0; j < 5; j++)    // 모든 x 위치
+                if (place[i][j] == 'P')    // x, y 위치에 사람일 경우
+                    for (auto dir1 : dirs) // 방향 한 곳을 보고
+                    {
+                        r = i + dir1.first;
+                        c = j + dir1.second;
+                        if (r < 0 || c < 0 || r >= 5 || c >= 5 || place[r][c] == 'X') // 맵 밖이거나 벽이면 패스
+                            continue;
 
-        // 노드 전부 보기
-        while (!q.empty())
-        {
-            node virus = q.front();
-            q.pop();
-        }
+                        if (place[r][c] == 'P') // 사람일 경우 확진 판정
+                        {
+                            ans = 0;
+                            goto FINISH;
+                        }
+                        for (auto dir2 : dirs) // 다음 방향 보기
+                        {
+                            r = i + dir1.first + dir2.first;
+                            c = j + dir1.second + dir2.second;
+                            if (r == i && c == j) // 뒤로 돌아갈 경우 패스
+                                continue;
+                            if (r < 0 || c < 0 || r >= 5 || c >= 5 || place[r][c] == 'X') // 맵 밖이거나 벽이면 패스
+                                continue;
+
+                            if (place[r][c] == 'P') // 사람일 경우 확진 판정
+                            {
+                                ans = 0;
+                                goto FINISH;
+                            }
+                        }
+                    }
+    FINISH:
+        answer.push_back(ans);
     }
 
     return answer;
