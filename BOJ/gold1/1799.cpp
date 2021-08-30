c543379c3047f84f55b2f533355d3c3b6538a9d1
GyeonghoYang <chchch1213@naver.com>
2021-08-30 16:01:28 +0900

    fin 1799

--- BOJ/gold1/1799.cpp
+++ BOJ/gold1/1799.cpp
@@ -6,86 +6,61 @@ using namespace std;
 typedef pair<int, int> pii;
 
 int n, ans = 0;
-vector<pii> points, bishops;
-// [0]...: 좌하[0] ~ 우상[198] \
-// [1]...: 좌상[0] ~ 우하[198] /
-// ...[x]: x번째 대각선
-int globalDiag[2][199] = {0}, curDiag[2][199];
+bool diag[2][199] = {0};
 
-bool promising(int (*diag)[199], pii p)
-{
-    return !(diag[0][p.first - p.second + n - 1] ||
-             diag[1][p.first + p.second]);
-}
-void place(int (*diag)[199], pii p)
-{
-    diag[0][p.first - p.second + n - 1]++;
-    diag[1][p.first + p.second]++;
-}
-void unplace(int (*diag)[199], pii p)
-{
-    diag[0][p.first - p.second + n - 1]--;
-    diag[1][p.first + p.second]--;
-}
-
-void backtrack(int idx)
+int dfs(vector<pii> &points, int idx)
 {
     if (idx >= points.size())
-        return;
+        return 0;
 
-    // 첫째줄에 대하여 n^2 으로 탐색함
-    if (points[idx].first == 0)
+    int result = 0;
+
+    int r = points[idx].first;
+    int c = points[idx].second;
+
+    bool &d1 = diag[0][r - c + n - 1];
+    bool &d2 = diag[1][r + c];
+
+    // 놓을 수 있는 경우
+    if (!d1 && !d2)
     {
-        // 비숍을 두고 백트래킹 진행
-        place(globalDiag, points[idx]);
-        bishops.push_back(points[idx]);
-        backtrack(idx + 1);
+        // 대각선 둘을 놓을 수 없다고 변경
+        d1 = d2 = true;
 
-        // 비숍을 빼고 백트래킹 진행
-        unplace(globalDiag, bishops.back());
-        bishops.pop_back();
-        backtrack(idx + 1);
-    }
-    // 둘째 줄일 경우 그냥 쭉 탐색하여 정답 갱신
-    else
-    {
-        copy(&globalDiag[0][0], &globalDiag[0][0] + 2 * 199, &curDiag[0][0]);
+        // 다음 포인트 탐색
+        result = dfs(points, idx + 1) + 1;
 
-        int num = bishops.size();
-        for (; idx < points.size(); idx++)
-            if (promising(curDiag, points[idx]))
-            {
-                num++;
-                place(curDiag, points[idx]);
-            }
-        if (num > ans)
-            ans = num;
+        // 다시 대각선 둘 놓을 수 있다고 변경
+        d1 = d2 = false;
     }
+
+    // 다음 포인트 탐색
+    return max(result, dfs(points, idx + 1));
 }
 
 int main(int argc, char *argv[])
 {
     cin.tie(0)->sync_with_stdio(0);
-    freopen(argv[1], "r", stdin); // For Test
+    // freopen(argv[1], "r", stdin); // For Test
 
     /* Input */
-    bool placeable;
+    vector<pii> black, white;
+    bool able;
     cin >> n;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
         {
-            cin >> placeable;
-            if (placeable)
-                points.push_back({i, j});
+            cin >> able;
+            if (!able)
+                continue;
+
+            if ((i + j) % 2)
+                white.push_back({i, j});
+            else
+                black.push_back({i, j});
         }
 
-    /* Process */
-    backtrack(0);
-    // 정단 갱신이 둘째줄 부터 이루어 지므로 첫째줄만 둔 경우를 대비해 정답 갱신
-    if (!points.empty() && ans == 0)
-        ans = points.size();
-
-    /* Output */
-    cout << ans << '\n';
+    /* Process & Output */
+    cout << dfs(black, 0) + dfs(white, 0) << '\n';
     return 0;
 }
