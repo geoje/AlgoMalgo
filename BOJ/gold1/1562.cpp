98c85de667c13d33fb9dd6d334f9d8a0bb87cbfe
GyeonghoYang <chchch1213@naver.com>
2021-09-07 15:23:44 +0900

    fin 1562

--- BOJ/gold1/1562.cpp
+++ BOJ/gold1/1562.cpp
@@ -1,9 +1,29 @@
 #include <cstdio> // For Test
 #include <iostream>
 using namespace std;
+typedef long long ll;
 
-// dp[length][last]
-long n, dp[101][10] = {0};
+const int MOD = 1e9;
+
+// dp[length: 길이][value: 시작하는 수][visible: 0~9까지 사용된 수 비트마스킹]
+ll n, dp[101][10][1 << 10];
+
+ll func(int len, int val, int vis)
+{
+    // 0또는 9에서 호출 되었을 때 OutOfRange 방지
+    if (val < 0 || val > 9)
+        return 0;
+
+    if (len == 1)
+        return (vis | (1 << val)) == ((1 << 10) - 1) ? 1 : 0;
+
+    ll &ret = dp[len][val][vis];
+    if (ret != -1)
+        return ret;
+
+    vis |= (1 << val);
+    return ret = (func(len - 1, val + 1, vis) + func(len - 1, val - 1, vis)) % MOD;
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,12 +32,14 @@ int main(int argc, char *argv[])
 
     /* Input */
     cin >> n;
+    fill_n(&dp[0][0][0], sizeof(dp) / sizeof(ll), -1);
 
     /* Process */
-    dp[10][0] = 1;
-    dp[10][9] = 1;
+    int ans = 0;
+    for (int i = 1; i <= 9; i++)
+        ans = (ans + func(n, i, 0)) % MOD;
 
     /* Output */
-    cout << n << '\n';
+    cout << ans << '\n';
     return 0;
 }
