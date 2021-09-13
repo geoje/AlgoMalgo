73cccd229fd2a9dd9893753338e510853bec42f0
GyeonghoYang <chchch1213@naver.com>
2021-09-13 13:09:24 +0900

    fin 1509

--- BOJ/gold1/1509.cpp
+++ BOJ/gold1/1509.cpp
@@ -5,44 +5,50 @@ using namespace std;
 const int INF = 2500;
 
 string str;
-bool isPalindrome[2500][2500];
+bool isPd[2500][2500] = {0};
 int dp[2500] = {0};
 
 int palindrome(int a, int b)
 {
-    // 메모이제이션 (a와 b가 같을 때와 1 차이 나서 범위 밖일 때는 Boundary Condition)
-    if (dp[a][b] != -1)
-        return dp[a][b];
-
-    // 내가 팰린드롬 일 경우
-    if (str[a] == str[b] && palindrome(a + 1, b - 1) == 1)
-        return dp[a][b] = 1;
-
-    // 아닐 경우 2개로 쪼갠 모든 경우를 보고 최소값 반환
-    int result = INF;
-    for (int m = a; m < b; m++)
-        result = min(result, palindrome(a, m) + palindrome(m + 1, b));
-
-    return dp[a][b] = result;
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
     cin.tie(0)->sync_with_stdio(0);
-    freopen(argv[1], "r", stdin); // For Test
+    // freopen(argv[1], "r", stdin); // For Test
 
     /* Input */
     cin >> str;
-
-    // Boundary Condition
-    isPalindrome[0][0] = 1;
-    for (int i = 1; i < str.length(); i++)
-        isPalindrome[i][i - 1] = isPalindrome[i][i] = 1;
+    int n = str.size();
 
     /* Process */
+    // 팰린드롬인지 배열 만들기
+    isPd[0][0] = true;
+    for (int i = 1; i < n; i++)
+        isPd[i][i - 1] = isPd[i][i] = true;
+
+    for (int k = 1; k < n; k++)
+        for (int i = k - 1, j = k; i >= 0 && j < n; i--, j++)
+            isPd[i][j] = isPd[i + 1][j - 1] && str[i] == str[j];
+    for (int k = 1; k < n; k++)
+        for (int i = k - 1, j = k + 1; i >= 0 && j < n; i--, j++)
+            isPd[i][j] = isPd[i + 1][j - 1] && str[i] == str[j];
+
+    // 팰린드롬 체크
+    dp[0] = 1;
+    for (int i = 1; i < n; i++)
+        if (isPd[0][i])
+            dp[i] = 1;
+        else
+        {
+            dp[i] = dp[i - 1] + 1;
+            for (int j = 1; j < i; j++)
+                if (isPd[j][i] && dp[j - 1] + 1 < dp[i])
+                    dp[i] = dp[j - 1] + 1;
+        }
 
     /* Output */
-    cout
-        << palindrome(0, str.length() - 1);
+    cout << dp[n - 1] << '\n';
     return 0;
 }
