6117299c8d4c4500a7f4a6f87e9f6b8cf38c47a8
GyeonghoYang <chchch1213@naver.com>
2021-10-05 14:01:24 +0900

    timeout 16566

--- BOJ/platinum5/16566.cpp
+++ BOJ/platinum5/16566.cpp
@@ -1,38 +1,39 @@
 #include <cstdio> // For Test
+#include <set>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 int n, m, k;
-int cards[4000000];
+set<int> cards;
 bool use[4000000] = {0};
 
 int main(int argc, char *argv[])
 {
     cin.tie(0)->sync_with_stdio(0);
-    freopen(argv[1], "r", stdin); // For Test
+    // freopen(argv[1], "r", stdin); // For Test
 
     /* Input */
+    int card;
     cin >> n >> m >> k;
     for (int i = 0; i < m; i++)
-        cin >> cards[i];
-    sort(cards, cards + m);
+    {
+        cin >> card;
+        cards.insert(card);
+    }
 
     /* Process */
-    int card;
     while (k--)
     {
         // 카드 입력
         cin >> card;
 
         // 카드 선택
-        int i = 0;
-        while (cards[i] <= card || use[i])
-            i++;
+        auto it = cards.upper_bound(card);
 
         // 카드 사용
-        cout << cards[i] << '\n';
-        use[i] = true;
+        cout << *it << '\n';
+        cards.erase(it);
     }
 
     /* Output */
