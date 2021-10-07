e34724c9961f522a966cf9225a0ca43226bcb491
GyeonghoYang <chchch1213@naver.com>
2021-10-07 10:02:12 +0900

    fin 1085

--- BOJ/bronze3/1085.cpp
+++ BOJ/bronze3/1085.cpp
@@ -2,19 +2,21 @@
 #include <iostream>
 using namespace std;
 
-int n;
+int x, y, w, h, ans = 500;
 
 int main(int argc, char *argv[])
 {
     cin.tie(0)->sync_with_stdio(0);
-    freopen(argv[1], "r", stdin); // For Test
+    // freopen(argv[1], "r", stdin); // For Test
 
     /* Input */
-    cin >> n;
-
-    /* Process */
+    cin >> x >> y >> w >> h;
+    ans = min(ans, x);
+    ans = min(ans, y);
+    ans = min(ans, w - x);
+    ans = min(ans, h - y);
 
     /* Output */
-    cout << n << '\n';
+    cout << ans << '\n';
     return 0;
 }
