d73a4207dafe4c40ea392919138f64a595837ef6
Gyeongho Yang <chchch1213@naver.com>
2024-03-18 18:02:36 +0900

    fix: Still in p11000

--- BOJ/category/pq/P11000.java
+++ BOJ/category/pq/P11000.java
@@ -1,22 +1,46 @@
 package BOJ.category.pq;
 
-import java.util.Scanner;
+import java.util.*;
+
+record Lecture(int start, int end) {}
 
 public class p11000 {
+    
     public static void main(String[] args) {
         // Init
         Scanner in = new Scanner(System.in);
         
         // Read
         int n = in.nextInt();
+        int[] s = new int[n];
+        int[] t = new int[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = in.nextInt();
+            t[i] = in.nextInt();
+        }
 
         // Write
-        int result = solution(n);
+        int result = solution(n, s, t);
         System.out.printf("%d%n", result);
         in.close();
     }
 
-    static int solution(int n) {
-        return n;
+    static int solution(int n, int[] s, int[] t) {
+        int result = 0;
+        List<Lecture> list = new ArrayList<>();
+        PriorityQueue<Lecture> pq = new PriorityQueue<Lecture>((a, b) -> a.end() - b.end());
+
+        for (int i = 0; i < n; i++) list.add(new Lecture(s[i], t[i]));
+        list.sort((a, b) -> a.start() - b.start());
+        
+        for (var l : list) {
+            pq.add(l);
+
+            while (!pq.isEmpty() && )
+        }
+
+        
+
+        return result;
     }
 }
